e9f331a667e80a60f37c9417afd4b6437eb0fed0
Hidefumi Tate <jhexp66@gmail.com>
2020-12-23 04:39:33 +0900

    change url

--- crawler.py
+++ crawler.py
@@ -5,8 +5,7 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 
 
-DOMEIN = 'https://www.jma.go.jp/jp/amedas_h/today-23232.html'
-
+URL = 'https://weather.yahoo.co.jp/weather/amedas/1d/23232.html?m=temp'
 
 def get_soup(url):
     r = requests.get(url)
@@ -20,21 +19,19 @@ def get_today(soup):
 
 def get_tabel(url):
     dflist = pd.read_html(url)
-    return dflist[4]
+    return pd.DataFrame(dflist) 
 
 
 
-def df_to_csv():
-    df = get_tabel(DOMEIN)
+def df_to_csv(url):
+    df = get_tabel(url)
     df.to_csv('data/data.csv')
 
 
 #--------debug--------------------------------
 
-df = get_tabel(DOMEIN)
-data = pd.read_csv('/Users/t4t3/Dropbox/tools/twomotor/data/data.csv', index_col= 0  )
-
+df = get_tabel(URL)
+# data = pd.read_csv('/Users/t4t3/Dropbox/tools/twomotor/data/data.csv', index_col= 0  )
 
 print(df)
 print("#"*100)
-print(data)
